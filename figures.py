c95643eb76e575f210b9e7ebb679010e507199b0
orbuzik <m06122005@gmail.com>
2024-07-02 13:37:22 +0300

    Create basic configuration

---
+++ figures.py
@@ -0,0 +1,29 @@
+
+
+class Figure:
+    pass
+
+
+# Ладья
+class Rook(Figure):
+    pass
+
+
+# Конь
+class Knight(Figure):
+    pass
+
+
+# Слон
+class Bishop(Figure):
+    pass
+
+
+# Король
+class King(Figure):
+    pass
+
+
+# Королева
+class Qween(Figure):
+    pass
---
+++ main.py
@@ -0,0 +1,10 @@
+from src import *
+from figures import *
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
